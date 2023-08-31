455d99618dd1a44becde6d08be6f0dd3f74bf0fe
HernanGabriel <hernan24744@gmail.com>
2023-08-31 03:41:02 -0300

    configuration createPool

---
+++ BackEnd/src/db.js
@@ -0,0 +1,18 @@
+import { createPool } from "mysql2/promise";
+import {
+  DB_DATABASE,
+  DB_HOST,
+  DB_PASSWORD,
+  DB_PORT,
+  DB_USER,
+} from "./settings.js";
+
+//object conection
+export const pool = createPool({
+  //parameters
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  port: DB_PORT,
+  database: DB_DATABASE,
+});
