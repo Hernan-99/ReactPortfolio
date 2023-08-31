98b76563a31013afdea03372af92ba6a329fb080
HernanGabriel <hernan24744@gmail.com>
2023-08-31 04:00:11 -0300

    layout and animations for section contact

---
+++ Frontend/src/sections/Contact.jsx
@@ -0,0 +1,10 @@
+import "../assets/styles/Contact.css";
+
+export const Contact = () => {
+  return (
+    <section className="container-contacto">
+      <h3 className="titulo-contacto">CONTACTO</h3>
+      <button className="enviar btn-enviar">Enviar email</button>
+    </section>
+  );
+};
