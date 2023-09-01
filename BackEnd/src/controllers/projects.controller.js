58fdd653d549970794a0f86c4c1c32a2226b42b1
HernanGabriel <hernan24744@gmail.com>
2023-09-01 20:19:37 -0300

    Logic for create a project  and implementation of error handling with try-catch

--- BackEnd/src/controllers/projects.controller.js
+++ BackEnd/src/controllers/projects.controller.js
@@ -12,7 +12,8 @@ export const getProjects = async (req, res) => {
 export const getProject = async (req, res) => {
   try {
     const [rows] = await pool.query(
-      "SELECT * FROM project WHERE id_project = ?"
+      "SELECT * FROM project WHERE id_project = ?",
+      [req.params.id]
     );
     if (!rows.length) {
       return res.status(404).send({ message: "Ops!, algo salio mal" });
@@ -24,8 +25,32 @@ export const getProject = async (req, res) => {
   }
 };
 
-export const postProject = (req, res) => {
-  res.send("create project");
+export const postProject = async (req, res) => {
+  const {
+    name_project,
+    description_project,
+    img_project,
+    link_demo,
+    link_code,
+  } = req.body;
+
+  try {
+    const [rows] = await pool.query(
+      "INSERT INTO project (name_project, description_project, img_project, link_demo, link_code, profile_project_id) VALUES (?,?,?,?,?,1)",
+      [description_project, img_project, link_demo, link_code, name_project]
+    );
+    res.send({
+      id: rows.insertId,
+      description_project,
+      img_project,
+      link_demo,
+      link_code,
+      name_project,
+    });
+  } catch (error) {
+    res.status(500).send({ message: "Ops!, ocurrio un error" });
+    console.log(error);
+  }
 };
 
 export const editProject = (req, res) => {
