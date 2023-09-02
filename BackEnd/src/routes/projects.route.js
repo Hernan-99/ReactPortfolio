9b54210f1f84af1bf70a3ed68a588ebd6c86b767
HernanGabriel <hernan24744@gmail.com>
2023-09-01 21:14:34 -0300

    update for routes projects

--- BackEnd/src/routes/projects.route.js
+++ BackEnd/src/routes/projects.route.js
@@ -1,15 +1,17 @@
 import { Router } from "express";
-import { getProjects } from "../controllers/projects.controller.js";
-import { getProject } from "../controllers/projects.controller.js";
-import { postProject } from "../controllers/projects.controller.js";
-import { editProject } from "../controllers/projects.controller.js";
-import { delProject } from "../controllers/projects.controller.js";
+import {
+  getProject,
+  getProjects,
+  postProject,
+  patchProject,
+  delProject,
+} from "../controllers/projects.controller.js";
 
 const router = Router();
 router.get("/projects", getProjects);
 router.get("/project/:id", getProject);
 router.post("/post/project", postProject);
-router.patch("/edit/project", editProject);
-router.delete("/del/project", delProject);
+router.patch("/edit/project/:id", patchProject);
+router.delete("/del/project/:id", delProject);
 
 export default router;
