e9192e4c365938c9a3999bb528338cf8a82646b7
HernanGabriel <hernan24744@gmail.com>
2023-09-02 09:00:07 -0300

    Implementation of logic for getting all social media and handling errors whit try-catch statements

--- BackEnd/src/controllers/social.controller.js
+++ BackEnd/src/controllers/social.controller.js
@@ -1,19 +1,26 @@
-export const getSocials = (req, res) => {
-  res.send("social all");
+import { pool } from "../db.js";
+export const getSocials = async (req, res) => {
+  try {
+    const [rows] = await pool.query("SELECT * FROM social");
+    res.send(rows);
+  } catch (error) {
+    res.status(500).json({ message: "Ops!, ocurrio un error" });
+    console.log(error);
+  }
 };
 
-export const getSocial = (req, res) => {
+export const getSocial = async (req, res) => {
   res.send("social for id");
 };
 
-export const postSocial = (req, res) => {
+export const postSocial = async (req, res) => {
   res.send("create social");
 };
 
-export const editSocial = (req, res) => {
+export const editSocial = async (req, res) => {
   res.send("edit social");
 };
 
-export const delSocial = (req, res) => {
+export const delSocial = async (req, res) => {
   res.send("delete social");
 };
