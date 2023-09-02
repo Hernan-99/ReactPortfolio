e2564fba58b87b670856a8ef24d37c7717d0a0a2
HernanGabriel <hernan24744@gmail.com>
2023-09-01 21:17:32 -0300

    Implementation of logic for update, delete, and error handling with Try-Catch statements

--- BackEnd/src/controllers/projects.controller.js
+++ BackEnd/src/controllers/projects.controller.js
@@ -41,11 +41,11 @@ export const postProject = async (req, res) => {
     );
     res.send({
       id: rows.insertId,
+      name_project,
       description_project,
       img_project,
       link_demo,
       link_code,
-      name_project,
     });
   } catch (error) {
     res.status(500).send({ message: "Ops!, ocurrio un error" });
@@ -53,10 +53,49 @@ export const postProject = async (req, res) => {
   }
 };
 
-export const editProject = (req, res) => {
-  res.send("project edit for id");
+export const patchProject = async (req, res) => {
+  const { id } = req.params;
+  const {
+    name_project,
+    description_project,
+    img_project,
+    link_demo,
+    link_code,
+  } = req.body;
+
+  try {
+    const result = await pool.query(
+      "UPDATE project SET name_project = IFNULL(?, name_project), description_project = IFNULL(?, description_project), img_project = IFNULL(?, img_project), link_demo = IFNULL(?, link_demo), link_code = IFNULL(?, link_code) WHERE id_project = ?",
+      [name_project, description_project, img_project, link_demo, link_code, id]
+    );
+
+    if (result.affectedRows) {
+      return res.status(404).send({ message: "Ops!, pryecto no encontrado" });
+    }
+
+    const [rows] = await pool.query(
+      "SELECT * FROM project WHERE id_project = ?",
+      [id]
+    );
+    res.json(rows[0]);
+  } catch (error) {
+    res.status(500).send({ message: "Ops!, ocurrio un error" });
+  }
 };
 
-export const delProject = (req, res) => {
-  res.send("project delete for id");
+export const delProject = async (req, res) => {
+  try {
+    const [result] = await pool.query(
+      "DELETE FROM project WHERE id_project = ?",
+      [req.params.id]
+    );
+    if (result.affectedRows <= 0) {
+      return res.status(404).json({ message: "Ops!, proyecto no encontrado " });
+    }
+    res.status(204);
+    res.send("Proyecto eliminado");
+  } catch (error) {
+    res.status(500).json({ message: "Ops!, algo salio mal" });
+    console.log(error);
+  }
 };
